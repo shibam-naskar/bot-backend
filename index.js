151ff89475724228758db9706e8c96fa471d3303
shibam-naskar <shibamnskar@gmail.com>
2023-07-23 16:11:40 +0530

    update

--- index.js
+++ index.js
@@ -5,21 +5,6 @@ const app = express();
 const port = 3001;
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server,{
-	cors: {
-		origin: "http://localhost:3000",
-		methods: ["GET","POST"]
-	}
-});
-
-app.get('/', (req, res) => {
-	res.send('<h1>Hello world</h1>');
-  });
-
-  server.listen(port, () => {
-	console.log('listening on : ',port);
-  });
 
 
 
@@ -51,34 +36,5 @@ client.on('message', message => {
 	}
 });
 
-
-
-io.on('connection', (socket) => {
-	console.log('a user connected',socket.id);
-	socket.on('disconnect', () => {
-	  console.log('user disconnected');
-	});
-
-	socket.on('connected', (data)=>{
-		console.log("connected to the server", data);
-		socket.emit("connect","server got connected");
-	})
-
-	client.on('qr', (qr) => {
-		console.log('QR RECEIVED', qr);
-		socket.emit("qr",qr);
-	});
-
-	client.on('ready', () => {
-		console.log('Client is ready!');
-		socket.emit("ready","Client is Ready")
-		
-	});
-
-	client.on("disconnected",()=>{
-		console.log("client Got Disconnected")
-		socket.emit("disconnect","Client is Ready")
-	})
-  });
 
 client.initialize();
